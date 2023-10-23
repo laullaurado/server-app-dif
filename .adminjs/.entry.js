9f65b9393dda4e29a9dd63b8d26113138a197f11
laullaurado <80075476+laullaurado@users.noreply.github.com>
2023-10-23 08:50:48 -0600

    Delete .adminjs directory

--- .adminjs/.entry.js
+++
@@ -1,5 +0,0 @@
-AdminJS.UserComponents = {}
-import GraficaShow from '../grafica-show'
-AdminJS.UserComponents.GraficaShow = GraficaShow
-import Dashboard from '../dashboard'
-AdminJS.UserComponents.Dashboard = Dashboard
